e44e8c1868ea2b891f8a51d71c1824383de2e6bb
Ben-glitch-cloud <BTLawrence44@gmail.com>
2023-05-29 23:50:57 +0100

    first question added to the quiz

--- quiz.js
+++ quiz.js
@@ -1,5 +1,43 @@
 // main file for Multiple Choice Quiz funations 
 
-const text = document.getElementById("text") 
+// file of questions 
+let arrayOfAnswers = ["1567", "1665", "1666", "1731"]
 
-text.style.color = 'blue';
+// first question
+let questionFireOfLondon = "What year did the create fire of London start?"
+
+// access main question area
+let MainQuizForm = document.getElementById('mainQuizForm')
+
+// first funaction
+function displayQuestion(question="no question found"){
+    let getQuestionsBox = document.getElementById("questionBox")
+    let newQuestion = document.createElement("p")
+    newQuestion.innerHTML = question
+    getQuestionsBox.appendChild(newQuestion)
+}
+
+displayQuestion(questionFireOfLondon)
+
+// loop through all potenal answers
+
+function displayAnswersToQuestions(AnswersList){
+    for(let i = 0; i < AnswersList.length; i++){
+
+        let newQuestionDiv = document.createElement("div") 
+        let newInput = document.createElement("input")
+        let newLabel = document.createElement('label')
+
+        newQuestionDiv.setAttribute("class", "questionFour")
+        newInput.setAttribute("type", "checkbox")
+        newLabel.innerHTML = AnswersList[i]
+
+        newQuestionDiv.appendChild(newInput)
+        newQuestionDiv.appendChild(newLabel)
+
+        MainQuizForm.appendChild(newQuestionDiv)
+    }
+}
+
+displayAnswersToQuestions(arrayOfAnswers)
+
