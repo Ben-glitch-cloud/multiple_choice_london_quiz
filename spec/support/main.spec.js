c916056e38d071f2d030d2022b512bece1fed3da
Ben-glitch-cloud <BTLawrence44@gmail.com>
2023-06-14 16:46:25 +0100

    adding testing to my web application

---
+++ spec/support/main.spec.js
@@ -0,0 +1,8 @@
+describe("first test", function(){
+    it("testing if jasmine is running correcty on the brower", function(){
+        expect(true).toBe(true);
+    })
+})
+
+// Command for starting the tests
+//npx jasmine-browser-runner serve
